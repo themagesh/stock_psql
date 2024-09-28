dc837a0edc8cd9b95ab224f882331465f235f7c5
themagesh <themagesh.v@gmail.com>
2024-09-28 08:27:30 +0530

    page scrolling

--- client/urls.py
+++ client/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('input/', views.input_data_view, name='input-data'),
-    path('success/', views.success_view, name='success'),  # Success page view
-    path('singleLine/', views.singleLine, name='ssingleLine'),
-    # path('register/', views.register, name='register')
-]
+    path('success/', views.success_view, name='success'), 
+    path('singleLine/', views.singleLine, name='singleLine')
+    ]
--- client/views.py
+++ client/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .forms import InputDataForm
 from .models import InputData
 
@@ -23,11 +24,25 @@ def success_view(request):
         'stocks': stocks,  # Pass the fetched data to the template
     }
     return render(request, 'success.html', context)
-def singleLine(request):
-    stocks = InputData.objects.all()  # Query the InputData model
-    context = {
-        
-        'stocks': stocks,  # Pass the fetched data to the template
-    }
-    return render(request, 'allstock.html', context)
 
+# def singleLine(request):
+#     stocks = InputData.objects.all()  # Query the InputData model
+#     context = {
+        
+#         'stocks': stocks,  # Pass the fetched data to the template
+#     }
+#     return render(request, 'allstock.html', context)
+
+def singleLine(request):
+    stock_list = InputData.objects.all().order_by('id') # Fetch all stock records
+    paginator = Paginator(stock_list,10)  # Show 10 stocks per pag
+    page_number = request.GET.get('page')
+    singleLine = paginator.get_page(page_number)
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    # Handle AJAX request here
+  # If it's an AJAX request, return only the new page content
+        return render(request, 'allstock.html', {'singleLine': singleLine})
+    return render(request, 'stocks.html', {'singleLine': singleLine})
+
+         
