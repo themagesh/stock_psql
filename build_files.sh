2f0963071c04242b840b626232f34ae20aad7fde
themagesh <themagesh.v@gmail.com>
2024-10-09 10:17:49 +0530

    app

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo 'BUILD START'
-python3.12 -m venv venv 
+python3 -m venv venv 
 source venv/bin/activate
-python3.12 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 echo 'BUILD END'
 # Install dependencies
-python3.12 -m pip install setuptools
+python3 -m pip install setuptools
 
 echo 'colit'
 # Collect static files without user input
-python3.12 manage.py makemigrations
-python3.12 manage.py migrate
-python3.12 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 
 
