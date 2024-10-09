6e2bef1e69a6216ff1b24574399357573a116e67
themagesh <themagesh.v@gmail.com>
2024-10-09 09:19:33 +0530

    apps

--- build_files.sh
+++ build_files.sh
@@ -8,21 +8,15 @@ then
     exit 1
 fi
 
-if ! command -v pip &> /dev/null
+if ! command -v pip3 &> /dev/null
 then
     echo "pip could not be found. Make sure you have specified a Python runtime in vercel.json."
     exit 1
 fi
 
-# Install requirements
-pip install -r requirements.txt
-
-# Your other build commands...
-
-
 # Install dependencies
-pip install setuptools
-pip install -r requirements.txt
+pip3 install setuptools
+pip3 install -r requirements.txt
 
 # Collect static files without user input
 python manage.py makemigrations
