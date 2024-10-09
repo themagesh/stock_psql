e900a10bd60e769077d41cc0800307ebec56df14
themagesh <themagesh.v@gmail.com>
2024-10-09 22:03:28 +0530

    app

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-echo 'BUILD START'
-python3 -m venv venv 
+set -e
+
+# Activate virtual environment
 source venv/bin/activate
-python3 -m pip install -r requirements.txt
-echo 'BUILD END'
+
 # Install dependencies
-python3 -m pip install setuptools
+pip install -r requirements.txt
 
-echo 'colit'
-# Collect static files without user input
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
-
+# Run Django collectstatic
+python manage.py collectstatic --noinput
 
