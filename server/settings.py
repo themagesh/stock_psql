560b0448a8f360f9fec6ca4cc1bf8f4537e95214
TheMagesh <154007310+themagesh@users.noreply.github.com>
2024-10-11 15:56:48 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY='your-generated-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ['*','stock-psql.onrender.com']
 
 
 
