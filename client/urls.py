f6afaece2e2d1be67e87a77fff48d43dcf1640a0
TheMagesh <154007310+themagesh@users.noreply.github.com>
2024-10-11 17:43:17 +0530

    Update urls.py

--- client/urls.py
+++ client/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('input/', views.input_data_view, name='input-data'),
     path('success/', views.success_view, name='success'), 
-    path('singleLine/', views.singleLine, name='singleLine'),
+    #path('singleLine/', views.singleLine, name='singleLine'),
     path('stock_price/', views.stock_prices_view, name='stock_prices_view'),
-    path('', views.home, name='home'),
+    path('', views.singleLine, name='singleLine'),
     path('stock/<str:stock_code>/', views.stock, name='stock'), 
     path('search-stocks/', views.search_stocks, name='search_stocks')
 ]
